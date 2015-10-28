f2356f8d7975ef829d1935a3948128d0f6295042
Philip Waller <p.waller@uq.edu.au>
2015-10-28 15:45:44 +1000

    Added commandline tools to 'properties' file.
    The properties file is read into the EngineSettings bean via 'smartass-data.xml'.
    The bean stores the values in the project Preferences where they are read by
    the DviPdfCreator class. In future a platform specific properties file will
    allow for easier configurations.

--- src/main/java/au/edu/uq/smartass/web/composer/DviPdfCreator.java
+++ src/main/java/au/edu/uq/smartass/web/composer/DviPdfCreator.java
@@ -18,10 +18,15 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Map;
 import java.util.prefs.Preferences;
 import java.util.zip.ZipOutputStream;
 
 import au.edu.uq.smartass.web.Zip;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 /**
  * The DviPdfCreator class that creates PDF or DVI
@@ -29,14 +34,36 @@ import au.edu.uq.smartass.web.Zip;
  *
  */
 public class DviPdfCreator {
-	File output_path;
-	String tex_path;
-	
-	public DviPdfCreator() {
-		Preferences prefs = Preferences.userRoot().node("au/edu/uq/smartass");
-		output_path = new File(prefs.get("output_path", ""));
-		tex_path = prefs.get("tex_path", ".");
-	}
+
+	/** Class logger. */
+	private static final Logger LOG = LoggerFactory.getLogger( DviPdfCreator.class );
+
+        /** */
+        private File out_path;
+
+        /** */
+        private String tex_path;
+
+        /** */
+        private String latex_cmd;
+
+        /** */
+        private String pdflatex_cmd;
+
+        /** */
+        {
+                Preferences preferences = Preferences.userRoot().node("au/edu/uq/smartass");
+                
+                out_path = new File( preferences.get("output_path", "") );
+                tex_path = preferences.get("tex_path", ".");
+                latex_cmd = preferences.get("latex_command", "/usr/bin/latex");
+                pdflatex_cmd = preferences.get("pdflatex_command", "/usr/bin/pdflatex");
+
+                // @TODO: check for existance of Process Commands on system.
+                // @TODO: *     'latex'
+                // @TODO: *     'pdflatex'
+        }
+
 	
 	/**
 	 * Creates PDFs from the template
@@ -45,13 +72,13 @@ public class DviPdfCreator {
 	 * @throws IOException
 	 */
 	public void makePdfs(String texPath)  throws IOException {
-		File workPath = new File(output_path, texPath);
-		runLatex("pdflatex", workPath, "questions.tex");
-		runLatex("pdflatex", workPath, "questions.tex");
-		runLatex("pdflatex", workPath, "answers.tex");
-		runLatex("pdflatex", workPath, "answers.tex");
-		runLatex("pdflatex", workPath, "solutions.tex");
-		runLatex("pdflatex", workPath, "solutions.tex");
+		File workPath = new File(out_path, texPath);
+		runLatex(pdflatex_cmd, workPath, "questions.tex");
+		runLatex(pdflatex_cmd, workPath, "questions.tex");
+		runLatex(pdflatex_cmd, workPath, "answers.tex");
+		runLatex(pdflatex_cmd, workPath, "answers.tex");
+		runLatex(pdflatex_cmd, workPath, "solutions.tex");
+		runLatex(pdflatex_cmd, workPath, "solutions.tex");
 		
 		zipFiles(workPath, "pdf");
 	}
@@ -63,36 +90,39 @@ public class DviPdfCreator {
 	 * @throws IOException
 	 */
 	public void makeDvis(String texPath)  throws IOException {
-		File workPath = new File(output_path, texPath);
-		runLatex("latex", workPath, "questions.tex");
-		runLatex("latex", workPath, "answers.tex");
-		runLatex("latex", workPath, "solutions.tex");
+                File workPath = new File(out_path, texPath);
+		runLatex(latex_cmd, workPath, "questions.tex");
+		runLatex(latex_cmd, workPath, "answers.tex");
+		runLatex(latex_cmd, workPath, "solutions.tex");
 		
 		zipFiles(workPath, "dvi");
 	}
 	
 	/**
-	 * Executes external  (not java) routines to create DVI/PDF from the LaTeX code
+	 * Executes external (not java) routines to create DVI/PDF from the LaTeX code
 	 *   
-	 * @param latex_exec		the name of the latex executable
-	 * @param workDir			work directory
-	 * @param tex_name			the name of LaTeX file to be processed by latex executable
+	 * @param latex_exec    the name of the latex executable
+	 * @param workDir	work directory
+	 * @param tex_name	the name of LaTeX file to be processed by latex executable
 	 * @throws IOException
 	 */
 	public void runLatex(String latex_exec, File workDir, String tex_name) throws IOException {
-		Process p = Runtime.getRuntime().exec(latex_exec + " -interaction=batchmode \""
-				+ tex_name + "\"", 
-			new String[] {"TEXINPUTS=" + tex_path},
-			workDir); 
-		BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
-		try {
-			String line;
-			while ((line = input.readLine()) != null) {
-				System.out.println(line);
-			}
-		} finally {
-			input.close();
-		}
+                LOG.info("::runLatex [ latex_exec=>{}, working_directory=>{}, tex_name=>{} ]", latex_exec, workDir, tex_name);
+                ProcessBuilder builder = new ProcessBuilder(
+                                latex_exec,
+                                "-interaction=batchmode",
+                                "\"" + tex_name + "\""
+                        );
+                Map<String,String> environment = builder.environment();
+                environment.put("TEXINPUTS", tex_path);
+                builder.directory(workDir);
+
+                Process process = builder.start();
+
+                BufferedReader processOut = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                String line;
+                while ( (line = processOut.readLine()) != null)
+                                LOG.info("::runLatex [output]=> {}", line);
 	}
 	
 	/**
