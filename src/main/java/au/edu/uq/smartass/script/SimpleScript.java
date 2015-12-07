491b4be3b1dc9a1dbaef5e66c0d6ac4ee56f6d66
Philip Waller <p.waller@uq.edu.au>
2015-12-07 13:41:24 +1000

    Removed old 'MathsModule' and fixded up references to it.

--- src/main/java/au/edu/uq/smartass/script/SimpleScript.java
+++ src/main/java/au/edu/uq/smartass/script/SimpleScript.java
@@ -125,7 +125,6 @@ public class SimpleScript extends Script {
 	 * @param name		the name for the variable
 	 * @param params	module constructor parameters
 	 */
-        @SuppressWarnings("unchecked")
 	private void createVar(String type, String name, String[] params) {
 		LOG.info( "::createVar()[ type=>{}, name=>{}, params=>{} ]", type, name, Arrays.toString(params) );
 		if ( ! vars.containsKey(name) ) vars.put(name, engine.getQuestionModule(type, params));
