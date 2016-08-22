4264e3e2b7f4349e27557b32df2513556f857e6b
uqrport1 <r.portas@uq.edu.au>
2016-08-23 09:09:36 +1000

    Fixed build script and added dep script

---
+++ util/get_deps.sh
@@ -0,0 +1,14 @@
+# Gets the dependencies for smartass
+COBRA="cobra-0.98.4"
+COBRA_ZIP="cobra-0.98.4.zip"
+
+mkdir ../lib
+
+# Setup cobra
+curl -O http://jaist.dl.sourceforge.net/project/xamj/Cobra%20HTML%20Toolkit/0.98.4/cobra-0.98.4.zip
+unzip $COBRA_ZIP
+cp $COBRA/lib/*.jar ../lib/
+
+# Setup jDvi
+curl -O https://www-sfb288.math.tu-berlin.de/jdvi/jDvi.jar
+cp jDvi.jar ../lib/
